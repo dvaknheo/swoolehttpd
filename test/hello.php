fbd41f64d95106b58065f7243911aaf0582d1e4e
dvakheo <dvaknheo@gmail.com>
2019-03-13 16:56:11 +0800

    first move!

---
+++ test/hello.php
@@ -0,0 +1,17 @@
+<?php
+require ('../src/SwooleHttpd.php');
+use DNMVCS\SwooleHttpd;
+
+function hello()
+{
+	echo "<h1> hello ,have a good start.</h1><pre>\n";
+	var_export(SwooleHttpd::SG());
+	echo "</pre>";
+    return true;
+}
+
+$options=[
+    'port'=>9528,
+    'http_handler'=>hello,
+];
+SwooleHttpd::RunQuickly($options);
